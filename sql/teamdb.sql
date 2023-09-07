89e665405e63ac3945d9da9a7c49f2bf9b44c0f4
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2023-09-07 11:10:48 +0900

    sql

--- sql/teamdb.sql
+++ sql/teamdb.sql
@@ -3,10 +3,10 @@ USE teamdb;
 
 CREATE TABLE IF NOT EXISTS teamdb_table
 ('id'       int(11) NOT NULL AUTO_INCREMENT,
-'name' text,
-PRIMARY KEY ('id'),
 'question' text,
 PRIMARY KEY ('id'));
 
 INSERT INTO teamdb_table (question) VALUES("sample1");
 INSERT INTO teamdb_table (question) VALUES("sample2");
+INSERT INTO teamdb_table (question) VALUES("sample3");
+INSERT INTO teamdb_table (question) VALUES("sample4");
