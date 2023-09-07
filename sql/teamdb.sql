8113bfd4a126b8cd15cb2f4ad5a3afd7b8f52ffb
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2023-09-07 13:07:41 +0900

    sqlを力技で入れる

--- sql/teamdb.sql
+++ sql/teamdb.sql
@@ -2,11 +2,10 @@ CREATE DATABASE IF NOT EXISTS teamdb;
 USE teamdb;
 
 CREATE TABLE IF NOT EXISTS teamdb_table
-('id'       int(11) NOT NULL AUTO_INCREMENT,
-'question' text,
-PRIMARY KEY ('id'));
+(
+  `id`         int(11) NOT NULL AUTO_INCREMENT,
+  `sample`     text,
+  PRIMARY KEY (`id`)
+);
 
-INSERT INTO teamdb_table (question) VALUES("sample1");
-INSERT INTO teamdb_table (question) VALUES("sample2");
-INSERT INTO teamdb_table (question) VALUES("sample3");
-INSERT INTO teamdb_table (question) VALUES("sample4");
+INSERT INTO teamdb_table (sample) VALUES ("sample1");
