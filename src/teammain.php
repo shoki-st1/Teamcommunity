dd0b7d893a25a84807d9e293aec519913b376a3e
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2023-09-14 11:54:09 +0900

    第一段

--- src/teammain.php
+++ src/teammain.php
@@ -3,6 +3,43 @@
 require_once 'header.php';
 ?>
 
+<!--タイトル-->
+<h1>メイン</h1>
+<hr>
+<!--各機能選択ボタンそれぞれ-->
+<div class="button-select">
+    <!--再帰-->
+    <!-- <button class="button" name="question" onclick="goQuestion()">質問一覧</button>
+
+        <button class="button" name="group" onclick="goGroup()">グループ</button>
+
+        <button class="button" name="talk" onclick="goTalk()">チャット</button> -->
+
+    <button class="button" name="log" onclick="goLog()">履歴</button>
+</div>
+<br>
+
+<!--JS-->
+<script>
+    // function goQuestion() {
+    //     window.location.href = "/quest/questDis.php";
+    // }
+
+    // function goGroup() {
+    //     window.location.href = "/quest/group.php";
+    //     document.write("grop");
+
+    // }
+
+    // function goTalk() {
+    //     window.location.href = "./quest/talk.php";
+    //     document.write("talk");
+    // }
+
+    function goLog() {
+        window.location.href = "./quest/log.php";
+    }
+</script>
 
 <!--質問へ飛ぶためのボタン-->
 <button class="button" onclick="question()">質問する</button>
