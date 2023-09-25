d0a0f101f611b087c2186feb809e7cb2a1755ee0
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2023-09-25 11:27:50 +0900

    login1

---
+++ src/login/adduserlogin.php
@@ -0,0 +1,5 @@
+<?php
+$username = $POST['name'];
+$userpass = $POST['pass'];
+
+require_once '../config.php';
--- src/login/userlogin.php
+++ src/login/userlogin.php
@@ -1,9 +1,21 @@
 <!--0J04007大江祥暉-->
 <!--通常ログイン時のphp処理-->
 <?php
+//受け取り代入
 $namestr = $_POST['name'];
 $password = $_POST['pass'];
 
+//データベースの情報をファイルから呼び出し
+require_once '../config.php';
+
+//データベース接続
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("接続に失敗しました");
+} else {
+    echo "ログイン";
+}
 
 
 ?>
