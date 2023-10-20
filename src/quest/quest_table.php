95766b1c9dfc9734bdb9dc5011ef14d8e944cc93
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2023-10-20 12:02:41 +0900

    sql,登録、削除、一覧表示、完了

--- src/quest/quest_table.php
+++ src/quest/quest_table.php
@@ -1,5 +1,5 @@
-<h1>やることを入力してください</h1>
 <!--登録をする画面-->
+<h1>やることを入力してください</h1>
 <form method="POST" action="quest_tablecheck.php">
     <!--上からタスク、開始日、終了日-->
     <input type="text" name="question" spellcheck="true" placeholder="100文字まで入力可能" maxlength="100">
